e5a71c95fb2d314b5dddb42a0caa30e0d369e58d
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2024-06-01 23:03:31 +0900

    Update standard.blade.php

--- views/standard.blade.php
+++ views/standard.blade.php
@@ -1,3 +1,10 @@
 <b>{{ $appName }}</b> ({{ $level_name }})
-Env: {{ $appEnv }}
-[{{ $datetime->format('Y-m-d H:i:s') }}] {{ $appEnv }}.{{ $level_name }} {{ $formatted }}
+[{{ $datetime->format('Y-m-d H:i:s') }}] {{ $appEnv }}.{{ $level_name }}
+
+{!! Str::limit($message, 2000) !!}
+@if (!empty($context))
+  <pre>{{ json_encode($context, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) }}</pre>
+@endif
+@if (!empty($exception))
+  <pre><code class="language-log">{{ $exception }}</code></pre>
+@endif
