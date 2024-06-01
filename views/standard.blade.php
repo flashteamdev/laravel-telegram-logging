49afa0b457f31a2d3ed68656f0aae9aa73b8fcb7
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2024-06-01 23:38:43 +0900

    Update standard.blade.php

--- views/standard.blade.php
+++ views/standard.blade.php
@@ -1,5 +1,4 @@
-<b>{{ $appName }}</b> ({{ $level_name }})
-[{{ $datetime->format('Y-m-d H:i:s') }}] {{ $appEnv }}.{{ $level_name }}
+[{{ $datetime->format('Y-m-d H:i:s') }}] <b>{{ $appName }}</b> {{ $appEnv }}.{{ $level_name }}
 
 {!! Str::limit($message, 2000) !!}
 @if (!empty($context))
