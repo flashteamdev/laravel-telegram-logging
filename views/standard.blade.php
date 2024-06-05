c4d0a8d03f3794088bd503b4c6f2c1462ee7f93a
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2024-06-05 11:23:16 +0900

    xóa datetime vì tele đã có hiện rồi

--- views/standard.blade.php
+++ views/standard.blade.php
@@ -1,5 +1,3 @@
-[{{ $datetime->format('Y-m-d H:i:s') }}] <b>{{ $appName }}</b> {{ $appEnv }}.{{ $level_name }}
-
 {!! Str::limit($message, 2000) !!}
 @if (!empty($context))
   <pre>{{ json_encode($context, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) }}</pre>
@@ -7,3 +5,4 @@
 @if (!empty($exception))
   <pre><code class="language-log">{{ $exception }}</code></pre>
 @endif
+[{{ $datetime->format('Y-m-d H:i:s') }}] <b>{{ $appName }}</b> {{ $appEnv }}.{{ $level_name }}
